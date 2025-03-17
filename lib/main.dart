8663998daaf0bf234987bba03384dfcdcc17076e
Motasim Fuad <motasimfuad99@gmail.com>
2025-03-17 22:12:56 +0600

    Updated project for web deployment

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Motasim Fuad Portfolio',
 
       translations: Languages(),
       locale: Locale("en","US"),
